d1ab6ffee972525376b3912e5f77dd67330d1bd0
Aluno <Aluno@sn633>
2019-11-29 02:23:58 -0200

    clima 2

--- public_html/js/clima.js
+++ public_html/js/clima.js
@@ -1,7 +1,9 @@
+
 function getClima(){
     $.ajax({
-        method: 'get', crossDomain: true, url: 'http://api.openweathermap.org/data/2.5/weather?id=3468879&appid=5a9a1b306ac7e38c785f3018082af4ca',
-        dataType: 'json', success: function(data){
+        method: 'get', crossDomain: true, url: 'https://api.openweathermap.org/data/2.5/weather?id=3468879&appid=971c8189ee4c574353095502279a73ff',
+        dataType: 'json', 
+        success: function(data){
             
             descricao = traduzirDescricao(data.weather[0].description);
             $('#situacao').html(descricao);
@@ -17,15 +19,29 @@ function getClima(){
             $('#velocidadeVento').html(velocidadeVento + "");
             
             tempMin = data.main.temp_min - 273;
-            var temp2Formatada = temperatura.toFixed(0).split('.');
+            var temp2Formatada = tempMin.toFixed(0).split('.');
             $('#tempMin').html(temp2Formatada + "°C");
             
             tempMax = data.main.temp_max - 273;
-            var temp3Formatada = temperatura.toFixed(0).split('.');
-            $('#tempMax').html(temp3Formatada + "°c");
+            var temp3Formatada = tempMax.toFixed(0).split('.');
+            $('#tempMax').html(temp3Formatada + "°C");
             
             pressaoVento = data.wind.deg;
             $('#pressaoVento').html(pressaoVento);
+            
+            var dataAmanhecer = new Date(data.sys.sunrise*1000);
+            var descDataAmanhecer = dataAmanhecer.getHours()+':'+dataAmanhecer.getMinutes();
+            $('#amanhecer').html(descDataAmanhecer);
+            
+            
+              var dataAnoitecer = new Date(data.sys.sunset*1000);
+            var descDataAnoitecer = dataAnoitecer.getHours()+':'+dataAnoitecer.getMinutes();
+            $('#Anoitecer').html(descDataAnoitecer);
+            
+            
+            var icone = data.weather[0].icon;
+            var caminhoIcone = 'img/icones/'+icone+'.png';
+            $('#icone').attr('src', caminhoIcone);
         }, 
         error: function (argument) {
             alert('Falha ao obter dados!');
